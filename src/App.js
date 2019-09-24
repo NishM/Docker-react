4e62507d1b9d6cdfcd8bec35a329098c4617420f
Nish <mnbdba@gmail.com>
2019-09-24 16:13:23 +0100

    added new js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to MyReact</h1>
-        <p>
+        <p className = "App-intro">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
